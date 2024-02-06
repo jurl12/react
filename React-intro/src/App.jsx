41b18dfd36c062ca6ded31ca7670620c39873220
jorge <andreselpinta2006@hotmail.com>
2024-02-06 13:42:02 -0500

    primer commit

---
+++ React-intro/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import { Example } from './components/Example/Example'
+
+
+
+
+
+function App() { 
+
+  return (
+    <>
+     <Example title = 'jorge andres'/>
+     <Example title='Andrés' />
+     <Example title='alo'/>
+    </>
+  )
+}
+
+export default App
---
+++ React-intro/src/components/Example/Example.jsx
@@ -0,0 +1,8 @@
+
+export const Example = ({title = 'holaa'}) => {
+  return (
+    <>
+        <h2>{title}</h2>
+    </>
+  )
+}
